746285c94aef3e27840c1498e64c9f7333984e84
Ashwani Upadhyay <avinash744826@gmail.com>
2026-07-21 11:12:58 +0530

    Time: 0 ms (100%), Space: 11.2 MB (58.83%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        stringstream ss(path);
+        string token;
+        
+        while (getline(ss, token, '/')) {
+            if (token == "" || token == ".") {
+                continue;
+            }
+            if (token == "..") {
+                if (!st.empty()) {
+                    st.pop_back();
+                }
+            } else {
+                st.push_back(token);
+            }
+        }
+        
+        string result = "";
+        for (const string& dir : st) {
+            result += "/" + dir;
+        }
+        
+        return result.empty() ? "/" : result;
+    }
+};
