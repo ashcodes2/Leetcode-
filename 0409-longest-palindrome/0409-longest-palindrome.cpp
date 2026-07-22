c030c5424f66adddf7f978334aef3fcd1f58b126
Ashwani Upadhyay <avinash744826@gmail.com>
2026-07-22 10:03:49 +0530

    Time: 0 ms (100%), Space: 9.2 MB (86.42%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> count;
+        for (char c : s) {
+            count[c]++;
+        }
+        
+        int length = 0;
+        bool hasOdd = false;
+        
+        for (auto& pair : count) {
+            if (pair.second % 2 == 0) {
+                length += pair.second;
+            } else {
+                length += pair.second - 1;
+                hasOdd = true;
+            }
+        }
+        
+        if (hasOdd) {
+            length += 1;
+        }
+        
+        return length;
+    }
+};
